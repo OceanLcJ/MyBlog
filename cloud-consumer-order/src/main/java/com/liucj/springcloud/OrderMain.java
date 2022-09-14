204057f0bd3dd6bbc06ce98494da1553708250f7
liucj <liucj@newland.com.cn>
2022-09-14 09:04:50 +0800

    消费者添加nacos

--- cloud-consumer-order/src/main/java/com/liucj/springcloud/OrderMain.java
+++ cloud-consumer-order/src/main/java/com/liucj/springcloud/OrderMain.java
@@ -2,8 +2,10 @@ package com.liucj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
